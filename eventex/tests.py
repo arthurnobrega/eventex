1f3d0bc468b0a3ac038a31032ce61197b99c4093
Arthur Nobrega <arthurthiago@gmail.com>
2015-12-30 20:36:54 -0200

    Adding tests to Home View

---
+++ eventex/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'index.html')
